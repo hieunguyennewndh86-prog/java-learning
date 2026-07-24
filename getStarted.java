7696c62701049c21a4d19327e25c697519b996c2
hieunguyen.new.ndh.86 <hieunguyen.new.ndh.86@gmail.com>
2026-07-24 16:45:55 +0900

    add readln function and assignthe read value to name variable, then print it to the display

--- getStarted.java
+++ getStarted.java
@@ -1,4 +1,5 @@
 void main() {
-IO.println("hi, java");
-
+  IO.println("hi, java"); // function for printing a text to screen
+  var name = IO.readln("what is your name? "); // function for read user input
+  IO.println("Hello " + name);
 }
